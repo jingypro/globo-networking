520a6112535a057af78f8d08e50173e94881cb7c
jyan <jingyan.pro@gmail.com>
2023-09-17 09:27:39 -0400

    Initial commit

---
+++ imports.tf
@@ -0,0 +1,50 @@
+##################################################################################
+# IMPORTS
+##################################################################################
+
+/*
+import {
+  to = module.main.aws_vpc.this[0]
+  id = "vpc-06bbcabb738ed2293" #VPC
+}
+
+import {
+  to = module.main.aws_subnet.public[0]
+  id = "subnet-0e6f48dde03e5d9d1" #PublicSubnet1
+}
+
+import {
+  to = module.main.aws_subnet.public[1]
+  id = "subnet-0dd02f003445389e8" #PublicSubnet2
+}
+
+import {
+  to = module.main.aws_internet_gateway.this[0]
+  id = "igw-04f4475df4c72030b" #InternetGateway
+}
+
+import {
+  to = module.main.aws_route.public_internet_gateway[0]
+  id = "rtb-0dfd93d7fc8206176_0.0.0.0/0" #DefaultPublicRoute
+}
+
+import {
+  to = module.main.aws_route_table.public[0]
+  id = "rtb-0dfd93d7fc8206176" #PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[0]
+  id = "subnet-0e6f48dde03e5d9d1/rtb-0dfd93d7fc8206176" #PublicSubnet1/PublicRouteTable
+}
+
+import {
+  to = module.main.aws_route_table_association.public[1]
+  id = "subnet-0dd02f003445389e8/rtb-0dfd93d7fc8206176" #PublicSubnet2/PublicRouteTable
+}
+
+import {
+  to = aws_security_group.ingress
+  id = "sg-0b492928a0c1df837" #NoIngressSecurityGroup
+}
+*/
