b7a41820b7189193bc78a24d397f1140ec3cceea
jyan <jingyan.pro@gmail.com>
2023-09-19 22:59:42 -0400

    Limit output to the first 2 public subnets

--- outputs.tf
+++ outputs.tf
@@ -7,7 +7,15 @@ output "vpc_id" {
   description = "VPC ID"
 }
 
+/*
 output "public_subnets" {
   value       = module.main.public_subnets
   description = "List of public subnets"
 }
+*/
+
+output "public_subnets_first_two" {
+  value       = slice(module.main.public_subnets, 0, 2)
+  description = "List of first two public subnets"
+}
+
